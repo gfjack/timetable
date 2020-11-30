14b806f62d6e9aab2be0305b29387e32fc09e453
simon <smh335789877@gmail.com>
2020-11-30 23:22:05 +0800

    添加 student service 接口

--- src/main/java/com/education/timetable/service/StudentService.java
+++ src/main/java/com/education/timetable/service/StudentService.java
@@ -1,5 +1,31 @@
 package com.education.timetable.service;
 
+import com.education.timetable.model.entity.StudentPo;
+import com.education.timetable.model.vo.StudentCreateVo;
+import com.education.timetable.model.vo.StudentRegisterVo;
+import com.education.timetable.model.vo.StudentUpdateVo;
+import com.education.timetable.model.vo.StudentVo;
+import com.education.timetable.model.vo.StudentWithdrawVo;
+
+import java.util.List;
+import java.util.Map;
+
 public interface StudentService {
+
+  StudentVo createStudent (StudentCreateVo studentCreateVo);
+
+  StudentVo getOne(String studentId);
+
+  List<StudentVo> getAll();
+
+  List<StudentVo> getBy(Map<String, Object> params);
+
+  boolean deleteOne(String studentId);
+
+  StudentVo UpdateOne(StudentUpdateVo studentUpdateVo);
+
+  StudentRegisterVo register(StudentRegisterVo studentRegisterVo);
+
+  StudentWithdrawVo withdraw(StudentWithdrawVo studentWithdrawVo);
 
 }
--- src/main/java/com/education/timetable/service/impl/StudentServiceImpl.java
+++ src/main/java/com/education/timetable/service/impl/StudentServiceImpl.java
@@ -1,9 +1,56 @@
 package com.education.timetable.service.impl;
 
+import com.education.timetable.model.vo.StudentCreateVo;
+import com.education.timetable.model.vo.StudentRegisterVo;
+import com.education.timetable.model.vo.StudentUpdateVo;
+import com.education.timetable.model.vo.StudentVo;
+import com.education.timetable.model.vo.StudentWithdrawVo;
 import com.education.timetable.service.StudentService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 
 @Service
 public class StudentServiceImpl implements StudentService {
+  @Override
+  public StudentVo createStudent(StudentCreateVo studentCreateVo) {
+    return null;
+  }
+
+  @Override
+  public StudentVo getOne(String studentId) {
+    return null;
+  }
+
+  @Override
+  public List<StudentVo> getAll() {
+    return null;
+  }
+
+  @Override
+  public List<StudentVo> getBy(Map<String, Object> params) {
+    return null;
+  }
+
+  @Override
+  public boolean deleteOne(String studentId) {
+    return false;
+  }
+
+  @Override
+  public StudentVo UpdateOne(StudentUpdateVo studentUpdateVo) {
+    return null;
+  }
+
+  @Override
+  public StudentRegisterVo register(StudentRegisterVo studentRegisterVo) {
+    return null;
+  }
+
+  @Override
+  public StudentWithdrawVo withdraw(StudentWithdrawVo studentWithdrawVo) {
+    return null;
+  }
 }
