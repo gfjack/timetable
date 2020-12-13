c9ef69c2aed9b0515b80bc4899d13f8ea607dcc8
古丰杰 jack-gu <gfjack321@gmail.com>
2020-12-13 22:21:38 +0800

    文档拦截器修复

--- src/main/java/com/education/timetable/config/interceptors/WebInterceptorConfig.java
+++ src/main/java/com/education/timetable/config/interceptors/WebInterceptorConfig.java
@@ -2,6 +2,7 @@ package com.education.timetable.config.interceptors;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import javax.annotation.Resource;
@@ -16,5 +17,12 @@ public class WebInterceptorConfig extends WebMvcConfigurationSupport {
     protected void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor)
                 .addPathPatterns("/v1/**");
+    }
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
